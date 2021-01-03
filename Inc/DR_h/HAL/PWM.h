0efd1022f7aa20cc0c420f9ab2b07120da9abef6
ianicknoejovich <ianicknoejovich@localhost>
2021-01-03 18:41:29 -0300

    empiezo con pwm. hay que revisar timer1.c

---
+++ Inc/DR_h/HAL/PWM.h
@@ -0,0 +1,19 @@
+/*
+ * PWM.h
+ *
+ *  Created on: Jan 3, 2021
+ *      Author: ianicknoejovich
+ */
+
+#ifndef DR_H_PERIPHERALS_PWM_H_
+#define DR_H_PERIPHERALS_PWm_H_
+
+#include <System.h>
+
+class PWM {
+	//TODO
+};
+
+
+
+#endif
